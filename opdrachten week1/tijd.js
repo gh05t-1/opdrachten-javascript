14dc1c5cf98abcd18429bbfd90c3d15c7766fab5
gh05t-1 <livalles.lma@gmail.com>
2024-09-12 11:58:19 +0200

    opdracht tijdreizen
    opdracht tijdreizen gemaakt

---
+++ opdrachten week1/tijd.js
@@ -0,0 +1,8 @@
+const date = new Date(1985, 6, 13);    
+const date2 = new Date(1955, 8, 24);
+const date3 = new Date(2015, 7, 32);
+const date4 = new Date(1885, 0, 5)
+console.log(date);
+console.log(date2);
+console.log(date3);
+console.log(date4);
